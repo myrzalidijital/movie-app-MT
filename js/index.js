d71120c49e545978d2763e0c9cd7c1d8a9e761ee
myrzalidijital <hanmataev@gmail.com>
2022-02-22 17:11:26 +0600

    initial

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('.movie').slick( {
+    slidesToShow:3,
+    // slidesToScroll:1,
+    centerModer:true,
+    centerPadding:'60px',
+    // autoplay:true,
+    // autoplaySpeed:1000,
+    //
+    // arrows:true,
+    dots: true
+
+})
